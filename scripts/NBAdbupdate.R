3507b35ba93b48f2e7d94dc0794fd28ead9d9491
iksnizez <jrbrzezin@gmail.com>
2024-11-08 10:25:24 -0500

    NBA dbUpdate adds anId

--- scripts/NBAdbupdate.R
+++ scripts/NBAdbupdate.R
@@ -82,6 +82,7 @@ currenthoopR <- hoopR::load_nba_player_box(seasons = c(2024,2025)) %>%
     mutate(join.names = tolower(stringr::str_replace_all(athlete_display_name,suffix.rep))) %>%
     distinct(athlete_id, .keep_all= TRUE)
 
+
 datadump <- left_join(currenthoopR, currentRoto, by="join.names")
 datadump <- left_join(datadump, currentNBA, by="join.names")
 datadump <- datadump %>% 
@@ -89,21 +90,53 @@ datadump <- datadump %>%
         hooprId = as.integer(athlete_id),
         nbaId = as.integer(PERSON_ID)
     ) %>%
-    select(-athlete_id, -PERSON_ID) %>%
+    select(-athlete_id, -PERSON_ID) 
+
+
+### add actnet to data
+conn <- harvestDBconnect(league = league)
+dbSendQuery(conn, "SET GLOBAL local_infile = true;")
+
+actnet <- dbGetQuery(conn, "SELECT * FROM actnetplayers;") %>% 
+    mutate(join.names = tolower(stringr::str_replace_all(player, suffix.rep))) %>%
+    select(join.names, playerId, abbr) %>% 
+    rename('actnetId'='playerId')
+
+datadump <- left_join(datadump, actnet, by="join.names") %>%
     rename(c(
         player=athlete_display_name, pos=athlete_position_abbreviation,joinName=join.names
     ))
 
-
-conn <- harvestDBconnect(league = league)
-dbSendQuery(conn, "SET GLOBAL local_infile = true;")
-
-
 # sending the data back to the db for updating the table
-dbx::dbxUpdate(conn, "players", datadump, where_cols = c("joinName"))
-## can be used when only adding new players, otherwise duplicates will be appended or fail on PK dup
+###### this doesn't add new players... 
+#dbx::dbxUpdate(conn, "players", datadump, where_cols = c("joinName"))
+
+###### can be used when only adding new players, otherwise duplicates will be appended or fail on PK dup
 #dbWriteTable(conn, name = "players", value= as.data.frame(datadump), row.names = FALSE, overwrite=FALSE, append=TRUE)
+
+# upsert
+dbx::dbxUpsert(conn, "players", datadump,  where_cols = c("joinName"))#, skip_existing = TRUE)
+
 dbSendQuery(conn, "SET GLOBAL local_infile = false;")
 dbDisconnect(conn)
+
+######
+##### REMOVE DUPLICATES - NOT REQUIRED ON EVERY RUN!
+#####
+#conn <- harvestDBconnect(league = league)
+#dbSendQuery(conn, "SET GLOBAL local_infile = true;")
+
+#data <- dbGetQuery(conn, "SELECT * FROM players;") %>%
+#    group_by(joinName) %>%
+#    arrange(desc(!is.na(actnetId))) %>%  # Prioritize non-null actnetId within each joinName group
+#    slice(1) %>%  # Keep the first record in each group after sorting
+#    ungroup()
+
+#dbWriteTable(conn, name = "players", value= as.data.frame(data), row.names = FALSE, ####overwrite=TRUE)
+
+#dbSendQuery(conn, "SET GLOBAL local_infile = false;")
+#dbDisconnect(conn)
+######
+
 #####
 
